ae559a72c31591c5d2c698817da2e5ca7a4c776f
joviannugeraha <joviandmi@gmail.com>
2022-03-11 16:13:44 +0700

    Percobaan DynamicVar

---
+++ src/main/kotlin/3_dynamicVar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+}
+
+//Berdasarkan contoh kode diatas dapat diketahui bahwa variabel staticVar memiliki tipe data
+//Char. Ketika kita memberikan nilai 34 pada staticVar maka compiler akan memberikan pesan
+//error.
