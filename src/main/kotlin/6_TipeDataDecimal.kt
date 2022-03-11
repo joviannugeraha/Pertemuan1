2768831a4d34677cb7dfa782cde7dcc6dfb2b904
joviannugeraha <joviandmi@gmail.com>
2022-03-11 16:38:58 +0700

    Percobaan LiteralConstant

--- src/main/kotlin/6_TipeDataDecimal.kt
+++ src/main/kotlin/6_TipeDataDecimal.kt
@@ -4,4 +4,3 @@ fun main(args: Array<String>) {
     println("maxFloat:" + maxFloat)
     println("maxDouble:" + maxDouble)
 }
-/** memperhatikan hasil presisi digit yang terekam oleh variabel maxFloat yaitu hanya 6 digit (kelebihan digit akan dibulatkan) sedangkan untuk var maxDouble presisi digit mencapai 10 digit. Untuk memberikan nilai sebagai float maka harus memberikan akhiran f atau F pada akhir literal.
