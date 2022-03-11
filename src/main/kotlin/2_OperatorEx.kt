697979e3cebe869e31f651a239eae343db2eea17
joviannugeraha <joviandmi@gmail.com>
2022-03-11 15:41:05 +0700

    Percobaan Operator

---
+++ src/main/kotlin/2_OperatorEx.kt
@@ -0,0 +1,23 @@
+fun main(){
+    var a=9
+    println(a++ - --a)
+}
+
+//pengerjaannya
+// 1. 9
+// 2. a = a+1
+//      = 9+1
+//      = 10
+//3. -
+//4. a = a-1
+//     = 10 - 1
+//     = 9
+//5. 9
+
+//Contoh komentar satu baris
+/*
+Contoh komentar multi baris
+*/
+/**
+ * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+ */
