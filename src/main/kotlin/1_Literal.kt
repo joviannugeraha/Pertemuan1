5562e0231092166f54e6120462fd90362fa70a3e
joviannugeraha <joviandmi@gmail.com>
2022-03-11 14:28:09 +0700

    Percobaan Literal

---
+++ src/main/kotlin/1_Literal.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //ini adalah deklarasi dan inisialisasi variable variable
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+    //menampilkan variable menggunakan string template
+    println("intLiteral =$intLiteral")
+    println("doubleLiteral =$doubleLiteral")
+    println("stringLiteral =$stringLiteral")
+    println("charLiteral =$charLiteral")
+    println("boolLiteral =$boolLiteral")
+}
